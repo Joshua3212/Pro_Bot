c196e1cf0ee5fbd161bb78a634a2c6905492ab2c
Joshua3212 <49119664+Joshua3212@users.noreply.github.com>
2019-04-07 13:58:34 +0200

    Create texte.py

---
+++ texte.py
@@ -0,0 +1,129 @@
+SPRACHEN = '''﻿Here you can see the languages I support :thumbsup:
+ ``` af = afrikaans
+    sq = albanian
+    am = amharic
+    ar = arabic
+    hy = armenian
+    az = azerbaijani
+    eu = basque
+    be = belarusian
+    bn = bengali
+    bs = bosnian
+    bg = bulgarian
+    ca = catalan
+    ceb = cebuano
+    ny = chichewa
+    zh-cn = chinese (simplified)
+    zh-tw = chinese (traditional)
+    co = corsican
+    hr = croatian
+    cs = czech
+    da = danish
+    nl = dutch
+    en = english
+    eo = esperanto
+    et = estonian
+    tl = filipino
+    fi = finnish
+    fr = french
+    fy = frisian
+    gl = galician
+    ka = georgian
+    de = german
+    el = greek
+    gu = gujarati
+    ht = haitian creole
+    ha = hausa
+    haw = hawaiian
+    iw = hebrew
+    hi = hindi
+    hmn = hmong
+    hu = hungarian
+    is = icelandic
+    ig = igbo
+    id = indonesian
+    ga = irish
+    it = italian
+    ja = japanese
+    jw = javanese
+    kn = kannada
+    kk = kazakh
+    km = khmer
+    ko = korean
+    ku = kurdish (kurmanji)
+    ky = kyrgyz
+    lo = lao
+    la = latin
+    lv = latvian
+    lt = lithuanian
+    lb = luxembourgish
+    mk = macedonian
+    mg = malagasy
+    ms = malay
+    ml = malayalam
+    mt = maltese
+    mi = maori
+    mr = marathi
+    mn = mongolian
+    my = myanmar (burmese)
+    ne = nepali
+    no = norwegian
+    ps = pashto
+    fa = persian
+    pl = polish
+    pt = portuguese
+    pa = punjabi
+    ro = romanian
+    ru = russian
+    sm = samoan
+    gd = scots gaelic
+    sr = serbian
+    st = sesotho
+    sn = shona
+    sd = sindhi
+    si = sinhala
+    sk = slovak
+    sl = slovenian
+    so = somali
+    es = spanish
+    su = sundanese
+    sw = swahili
+    sv = swedish
+    tg = tajik
+    ta = tamil
+    te = elugu
+    th = thai
+    tr = turkish
+    uk = ukrainian
+    ur = urdu
+    uz = uzbek
+    vi = vietnamese
+    cy = welsh
+    xh = xhosa
+    yi = yiddish
+    yo = yoruba
+    zu = zulu
+    fil = Filipino
+    he = Hebrew```'''
+
+
+HELP = '''**__Lottery__**
+just tipe in **-lottery** and guess a number between 1 and 30 :red:
+
+**__Tags__**
+they are given to u automaticly if u join ur if you go offline or online(just if u changed it before)
+
+**__Random responses__**
+**-rtm** (the surviv rtm links)
+**-MEE6** (just try xD)
+**@ProBot (WHO PINGED ME :rage:)
+...
+
+**__Translate funktion__**
+tipe in ..
+**-translate** to translate stuff from any language into english
+**-translatefromto** to translate stuff from your language into another language
+like ... -translatefromto en de I am the best discord bot
+
+If u have any question please ask @Joshua#7106
+**Good luck :thumbsup:'''
